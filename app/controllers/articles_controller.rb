97ca47cc53ac2a22d7b9e0f5121beece6d8ef527
Michael Houmann <mhoumann87@gmail.com>
2019-03-10 20:36:17 +0100

    Add update route and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,16 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    redirect_to article_path(@article)
+  end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
     redirect_to article_path(@article)
   end
 
